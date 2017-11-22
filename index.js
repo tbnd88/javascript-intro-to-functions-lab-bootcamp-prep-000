0db7dfdad73497cbda05e03c2c66b049e3e058f2
Tyler Tubbs <tubbs.tyler.2009@gmail.com>
2017-11-22 06:17:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,5 @@ function logWhisper(string)
 function sayHiToGrandma(string)
 {
   var upperCase = "HELLO!"
-  if (upperCase.toUpperCase === upperCase)
-  {return "YES INDEED!"}
+  console.log(string)
 }
